fa1211a3c7b16615cdfbbdb9e5257368c4c33502
Fredrik Ekre <fredrik.ekre@chalmers.se>
2018-06-07 14:42:54 +0200

    invokelatest on some methods

--- src/IJulia.jl
+++ src/IJulia.jl
@@ -3,6 +3,7 @@
 module IJulia
 using Compat.Base64
 import Compat: showable
+import Base: invokelatest
 
 const text_plain = MIME("text/plain")
 const image_svg = MIME("image/svg+xml")
@@ -18,24 +19,24 @@ const application_vnd_vegalite_v2 = MIME("application/vnd.vegalite.v2+json")
 # for passing to Jupyter display_data and execute_result messages.
 function display_dict(x)
     data = Dict{String,Any}("text/plain" => limitstringmime(text_plain, x))
-    if showable(application_vnd_vegalite_v2, x)
+    if invokelatest(showable, application_vnd_vegalite_v2, x)
         data[string(application_vnd_vegalite_v2)] = JSON.parse(limitstringmime(application_vnd_vegalite_v2, x))
     end
-    if showable(image_svg, x)
+    if invokelatest(showable, image_svg, x)
         data[string(image_svg)] = limitstringmime(image_svg, x)
     end
-    if showable(image_png, x)
+    if invokelatest(showable, image_png, x)
         data[string(image_png)] = limitstringmime(image_png, x)
-    elseif showable(image_jpeg, x) # don't send jpeg if we have png
+    elseif invokelatest(showable, image_jpeg, x) # don't send jpeg if we have png
         data[string(image_jpeg)] = limitstringmime(image_jpeg, x)
     end
-    if showable(text_markdown, x)
+    if invokelatest(showable, text_markdown, x)
         data[string(text_markdown)] = limitstringmime(text_markdown, x)
-    elseif showable(text_html, x)
+    elseif invokelatest(showable, text_html, x)
         data[string(text_html)] = limitstringmime(text_html, x)
-    elseif showable(text_latex, x)
+    elseif invokelatest(showable, text_latex, x)
         data[string(text_latex)] = limitstringmime(text_latex, x)
-    elseif showable(text_latex2, x)
+    elseif invokelatest(showable, text_latex2, x)
         data[string(text_latex)] = limitstringmime(text_latex2, x)
     end
     return data
@@ -57,7 +58,7 @@ function limitstringmime(mime::MIME, x)
             return String(x)
         else
             # show(IOContext(buf, :limit=>true, :color=>true), mime, x)
-            Base.invokelatest(show, IOContext(buf, :limit=>true, :color=>true), mime, x)
+            invokelatest(show, IOContext(buf, :limit=>true, :color=>true), mime, x)
         end
     else
         b64 = Base64EncodePipe(buf)
@@ -65,7 +66,7 @@ function limitstringmime(mime::MIME, x)
             write(b64, x) # x assumed to be raw binary data
         else
             # show(IOContext(b64, :limit=>true, :color=>true), mime, x)
-            Base.invokelatest(show, IOContext(b64, :limit=>true, :color=>true), mime, x)
+            invokelatest(show, IOContext(b64, :limit=>true, :color=>true), mime, x)
         end
         close(b64)
     end
